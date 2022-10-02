2ae8b952ea61de19d1164d693aada2b57a505bca
Alexander Hu <superalexanderhu@gmail.com>
2022-10-02 05:30:49 -0400

    remove unnecessary files

--- ImageSegmentation/ImageSegmentation/UIImageHelper.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-
-@interface UIImageHelper : NSObject
-
-- (UIImage *) convertRGBBufferToUIImage:(unsigned char *) buffer withWidth:(int)width withHeight:(int)height;
-@end
-
